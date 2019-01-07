010ea485137d8b79794cdb5c9f78c60460cc5101
pthianma <s5802041620165@email.kmutnb.ac.th>
2019-01-08 00:38:08 +0700

    add withdraw

--- withdraw.php
+++ withdraw.php
@@ -8,10 +8,53 @@
     <title>Index</title>
     <!-- InsertCss -->
     <?php include_once('insertCss.php'); ?>
+<style> 
+input[type=text] {
+  width: 80%;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url('searchicon.png');
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+  width: 80%;
+}
+</style>
 </head>
 <body class="Ebody">
     <?php
         include_once('connect.php');
+
+        if(isset($_POST['submit'])){
+            
+            $user_id =  $_POST['user_id'];
+
+            $sql = "SELECT * FROM `user` WHERE `user_id` = '".$user_id."' ";
+            $result = $conn->query($sql);
+
+            if($result->num_rows > 0){
+                // echo print_r($result);
+                $row = $result->fetch_assoc();
+
+                $_SESSION['reward'] = $row['reward'];
+                $_SESSION['user_id'] = $row['user_id'];
+                // echo $row['reward'];
+            }
+            else{
+                $_SESSION['reward'] = '(invalid) 0';
+                $_SESSION['user_id'] = 'invalid';
+
+            }
+        }
+
         include_once('header.php');
     ?>
     
@@ -23,8 +66,27 @@
                     <h1 class="display-4">Withdraw Money</h1>
                     </div>
                     <div class="card-body">
-                        <div class="container">
-                           
+                        <div class="container text-center">
+                            <form action="" method="POST">
+                                <!-- <div class="col-md-4">
+                                    <label for="user_id">Phone Number</label>
+                                    <input type="text" class="form-control" id="user_id" name="user_id">
+                                </div>
+                                <div class="col-md-2">
+                                    <input type="submit" name="submit" class="btn btn-success" value="Search">
+                                </div> -->
+                                <div class="text-center">
+                                <input type="text" id="user_id" name="user_id" placeholder="Search..">
+                                <input type="submit" name="submit" class="btn btn-success " value="Search"> 
+                                </div>
+                            </form>
+                            <?php  if(isset($_SESSION['user_id'])) { ?>                                
+                                <h4><b>Phone Number</b> : <?php  echo $_SESSION['user_id']; ?></h4>
+                                <h1 class="display-4 text-center"><?php  echo $_SESSION['reward']; ?> Bath</h1>
+                            </div>
+                            <?php }else { ?>
+                               
+                            <?php }?>
                         </div>
                     </div>
                 </div>
